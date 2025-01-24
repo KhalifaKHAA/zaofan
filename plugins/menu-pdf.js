3b9fe9607e404e0cf5bc44660b5267ff49f21093
KhalifaKHAA <khalifaalialtahery1@gmail.com>
2025-01-24 06:18:55 +0300

    menu-pdf.js

--- plugins/menu-pdf.js
+++ plugins/menu-pdf.js
@@ -16,7 +16,7 @@ const defaultMenu = {
   header: '╭─────≼ %category ≽',
   body: '╎≡ %cmd',
   footer: '╰┄┄┄┄┄┄┄┄┄┄┄┄┄〢',
-  after: 'BOBIZA 👑',
+  after: '𝒁𝒂𝒐𝒇𝒂𝒏 🐦‍🔥',
 }
 let handler = async (m, { conn, usedPrefix: _p, __dirname, args, command}) => {
 let tags = {
@@ -180,7 +180,7 @@ let tags = {
 	title: 'قسم خاص بكل ما له علاقة ب PDF ',
 	body: 'اليك الاوامر الخاصة بهذا القسم',
 	thumbnailUrl: 'https://telegra.ph/file/22455a32e324068fa1922.jpg',
-	sourceUrl: 'instagram.com/noureddine_ouafy',
+	sourceUrl: 'instagram.com/khalifaalialtahery',
 	mediaType: 1,
 	renderLargerThumbnail: true
 	}}})
@@ -191,7 +191,7 @@ let tags = {
 }
 handler.help = ['menu-pdf']
 handler.tags = ['categorie']
-handler.command = /menu-pdf$/i
+handler.command = /menu-pdf|اوامر-pdf$/i
 export default handler
 //----------- FUNCTION -------
 function pickRandom(list) {
